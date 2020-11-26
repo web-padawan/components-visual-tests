f1194f62e3537b8a1c35c59e678f589fd0e3155f
web-padawan <iamkulykov@gmail.com>
2020-11-26 21:57:38 +0200

    Add another visual tests suite

---
+++ test/visual/lumo/text-field.test.js
@@ -0,0 +1,33 @@
+import { visualDiff } from '../../../src/regression/browser/commands.mjs';
+import '@vaadin/vaadin-text-field';
+
+describe('visual tests', () => {
+  let div, element;
+
+  beforeEach(() => {
+    div = document.createElement('div');
+    div.style.padding = '10px';
+    div.style.display = 'inline-block';
+    element = document.createElement('vaadin-text-field');
+    div.appendChild(element);
+    document.body.appendChild(div);
+  });
+
+  afterEach(() => {
+    document.body.removeChild(div);
+  });
+
+  it('default', async () => {
+    await visualDiff(div, 'lumo-text-field-default');
+  });
+
+  it('disabled', async () => {
+    element.disabled = true;
+    await visualDiff(div, 'lumo-text-field-disabled');
+  });
+
+  it('label', async () => {
+    element.label = 'Label';
+    await visualDiff(div, 'lumo-text-field-label');
+  });
+});
