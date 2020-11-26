1102f2e2336c054a847f22e2a82e30c308520ddc
web-padawan <iamkulykov@gmail.com>
2020-11-26 15:57:38 +0200

    Cleanup web-test-runner config

--- web-test-runner.config.js
+++ web-test-runner.config.js
@@ -1,19 +1,10 @@
 const { webdriverIOLauncher } = require('./dist/wdio/index');
 const { createSauceLabsLauncher } = require('./dist/sauce/index');
-// const { createSauceLabsLauncher } = require('@web/test-runner-saucelabs');
 const { visualRegressionPlugin } = require('./dist/regression/index');
 
 const config = {
   nodeResolve: true
 };
-
-config.browsers = [
-  webdriverIOLauncher({
-    capabilities: {
-      browserName: 'Chrome'
-    }
-  })
-];
 
 if (process.env.TEST_ENV === 'visual' || process.env.TEST_ENV === 'update') {
   config.concurrency = 1;
@@ -34,14 +25,20 @@ const sauceLabsLauncher = createSauceLabsLauncher({
   key: process.env.SAUCE_ACCESS_KEY
 });
 
-/*
 config.browsers = [
+  webdriverIOLauncher({
+    capabilities: {
+      browserName: 'Chrome'
+    }
+  }),
+  /*
   sauceLabsLauncher({
     browserName: 'chrome',
     platformName: 'Windows 10',
     browserVersion: 'latest'
   })
+  */
 ];
-*/
+
 
 module.exports = config;
