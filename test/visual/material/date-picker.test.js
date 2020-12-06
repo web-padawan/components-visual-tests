7fcfab8bda3352742ab72ceb35aa9786fcb46f3d
web-padawan <iamkulykov@gmail.com>
2020-12-06 16:40:31 +0200

    Add date-picker and time-picker tests

---
+++ test/visual/material/date-picker.test.js
@@ -0,0 +1,90 @@
+
+import { visualDiff } from '../../../src/regression/browser/commands.mjs';
+import '@vaadin/vaadin-date-picker/theme/material/vaadin-date-picker.js';
+
+describe('date-picker', () => {
+  let div, element;
+
+  async function visualTest(name) {
+    await visualDiff(div, `date-picker:${name}`);
+  }
+
+  before(() => {
+    const range = new Range().createContextualFragment(
+      '<div style="padding: 10px; display: inline-block"></div>'
+    );
+    div = range.firstElementChild;
+    document.body.appendChild(div);
+  });
+
+  beforeEach(() => {
+    element = document.createElement('vaadin-date-picker');
+    div.appendChild(element);
+  });
+
+  afterEach(() => {
+    div.removeChild(element);
+  });
+
+  it('basic', async () => {
+    await visualTest('basic');
+  });
+
+  it('disabled', async () => {
+    element.disabled = true;
+    await visualTest('disabled');
+  });
+
+  it('readonly', async () => {
+    element.readonly = true;
+    await visualTest('readonly');
+  });
+
+  it('label', async () => {
+    element.label = 'Label';
+    await visualTest('label');
+  });
+
+  it('placeholder', async () => {
+    element.placeholder = 'Placeholder';
+    await visualTest('placeholder');
+  });
+
+  it('value', async () => {
+    element.value = '1991-12-20';
+    await visualTest('value');
+  });
+
+  it('required', async () => {
+    element.label = 'Label';
+    element.required = true;
+    await visualTest('required');
+  });
+
+  it('error message', async () => {
+    element.label = 'Label';
+    element.errorMessage = 'This field is required';
+    element.required = true;
+    element.validate();
+    await visualTest('error-message');
+  });
+
+  it('helper text', async () => {
+    element.helperText = 'Helper text';
+    await visualTest('helper-text');
+  });
+
+  it('clear button', async () => {
+    element.value = '1991-12-20';
+    element.clearButtonVisible = true;
+    await visualTest('clear-button');
+  });
+
+  it('prefix slot', async () => {
+    const span = document.createElement('span');
+    span.setAttribute('slot', 'prefix');
+    span.textContent = '$';
+    element.appendChild(span);
+    await visualTest('prefix');
+  });
+});
---
+++ test/visual/material/time-picker.test.js
@@ -0,0 +1,81 @@
+import { visualDiff } from '../../../src/regression/browser/commands.mjs';
+import '@vaadin/vaadin-time-picker/theme/material/vaadin-time-picker.js';
+
+describe('time-picker', () => {
+  let div, element;
+
+  async function visualTest(name) {
+    await visualDiff(div, `time-picker:${name}`);
+  }
+
+  before(() => {
+    const range = new Range().createContextualFragment(
+      '<div style="padding: 10px; display: inline-block"></div>'
+    );
+    div = range.firstElementChild;
+    document.body.appendChild(div);
+  });
+
+  beforeEach(() => {
+    element = document.createElement('vaadin-time-picker');
+    div.appendChild(element);
+  });
+
+  afterEach(() => {
+    div.removeChild(element);
+  });
+
+  it('basic', async () => {
+    await visualTest('basic');
+  });
+
+  it('disabled', async () => {
+    element.disabled = true;
+    await visualTest('disabled');
+  });
+
+  it('readonly', async () => {
+    element.readonly = true;
+    await visualTest('readonly');
+  });
+
+  it('label', async () => {
+    element.label = 'Label';
+    await visualTest('label');
+  });
+
+  it('placeholder', async () => {
+    element.placeholder = 'Placeholder';
+    await visualTest('placeholder');
+  });
+
+  it('value', async () => {
+    element.value = '12:12:12.122';
+    await visualTest('value');
+  });
+
+  it('required', async () => {
+    element.label = 'Label';
+    element.required = true;
+    await visualTest('required');
+  });
+
+  it('error message', async () => {
+    element.label = 'Label';
+    element.errorMessage = 'This field is required';
+    element.required = true;
+    element.validate();
+    await visualTest('error-message');
+  });
+
+  it('helper text', async () => {
+    element.helperText = 'Helper text';
+    await visualTest('helper-text');
+  });
+
+  it('clear button', async () => {
+    element.value = '12:12:12.122';
+    element.clearButtonVisible = true;
+    await visualTest('clear-button');
+  });
+});
