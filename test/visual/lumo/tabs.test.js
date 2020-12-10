396a5a74559ce69061770bad917e456dc411828c
web-padawan <iamkulykov@gmail.com>
2020-12-10 15:11:02 +0200

    Add item and tabs visual tests

---
+++ test/visual/lumo/tabs.test.js
@@ -0,0 +1,172 @@
+import '@vaadin/vaadin-tabs/vaadin-tabs.js';
+import { createTest, fixture } from '../utils.js';
+
+const screenshot = createTest('tabs');
+
+describe('tabs', () => {
+  let div;
+
+  describe('horizontal', () => {
+    before(() => {
+      div = fixture(`
+        <vaadin-tabs>
+          <vaadin-tab>Foo</vaadin-tab>
+          <vaadin-tab>Bar</vaadin-tab>
+          <vaadin-tab>Baz</vaadin-tab>
+          <vaadin-tab disabled>Disabled</vaadin-tab>
+        </vaadin-tabs>
+      `);
+      document.body.appendChild(div);
+    });
+
+    after(() => {
+      document.body.removeChild(div);
+    });
+
+    it('horizontal', async () => {
+      await screenshot(div, 'horizontal');
+    });
+
+    it('RTL horizontal', async () => {
+      await screenshot(div, 'rtl-horizontal');
+    });
+  });
+
+  describe('vertical', () => {
+    before(() => {
+      div = fixture(`
+        <vaadin-tabs orientation="vertical" style="width: 150px">
+          <vaadin-tab>Foo</vaadin-tab>
+          <vaadin-tab>Bar</vaadin-tab>
+          <vaadin-tab>Baz</vaadin-tab>
+          <vaadin-tab disabled>Disabled</vaadin-tab>
+          <vaadin-tab>Tab with text in two lines</vaadin-tab>
+          <vaadin-tab><a href="https://example.com">Link with text in two lines</a></vaadin-tab>
+        </vaadin-tabs>
+      `);
+      document.body.appendChild(div);
+    });
+
+    after(() => {
+      document.body.removeChild(div);
+    });
+
+    it('vertical', async () => {
+      await screenshot(div, 'vertical');
+    });
+
+    it('RTL vertical', async () => {
+      await screenshot(div, 'rtl-vertical');
+    });
+  });
+
+  describe('anchors', () => {
+    let element;
+
+    before(() => {
+      div = fixture(`
+        <vaadin-tabs>
+          <vaadin-tab><a href="#">Foo</a></vaadin-tab>
+          <vaadin-tab><a href="#">Bar</a></vaadin-tab>
+          <vaadin-tab><a href="#">Baz</a></vaadin-tab>
+        </vaadin-tabs>
+      `);
+      document.body.appendChild(div);
+      element = div.firstElementChild;
+    });
+
+    after(() => {
+      document.body.removeChild(div);
+    });
+
+    it('horizontal', async () => {
+      await screenshot(div, 'horizontal-anchors');
+    });
+
+    it('vertical', async () => {
+      element.orientation = 'vertical';
+      await screenshot(div, 'vertical-anchors');
+    });
+  });
+
+  describe('scroll', () => {
+    describe('horizontal scroll', () => {
+      before(() => {
+        div = fixture(`
+          <vaadin-tabs selected="8">
+            <vaadin-tab>Tab-00</vaadin-tab>
+            <vaadin-tab>Tab-01</vaadin-tab>
+            <vaadin-tab>Tab-02</vaadin-tab>
+            <vaadin-tab>Tab-03</vaadin-tab>
+            <vaadin-tab>Tab-04</vaadin-tab>
+            <vaadin-tab>Tab-05</vaadin-tab>
+            <vaadin-tab>Tab-06</vaadin-tab>
+            <vaadin-tab>Tab-07</vaadin-tab>
+            <vaadin-tab>Tab-08</vaadin-tab>
+            <vaadin-tab>Tab-09</vaadin-tab>
+            <vaadin-tab>Tab-10</vaadin-tab>
+            <vaadin-tab>Tab-11</vaadin-tab>
+            <vaadin-tab>Tab-12</vaadin-tab>
+            <vaadin-tab>Tab-13</vaadin-tab>
+            <vaadin-tab>Tab-14</vaadin-tab>
+            <vaadin-tab>Tab-15</vaadin-tab>
+          </vaadin-tabs>
+        `);
+        div.style.width = '400px';
+        document.body.appendChild(div);
+      });
+
+      after(() => {
+        document.body.removeChild(div);
+      });
+
+      it('horizontal scroll', async () => {
+        await screenshot(div, 'horizontal-scroll');
+      });
+
+      it('RTL horizontal scroll', async () => {
+        await screenshot(div, 'rtl-horizontal-scroll');
+      });
+    });
+
+    describe('vertical scroll', () => {
+      before(() => {
+        div = fixture(`
+          <vaadin-tabs selected="8" orientation="vertical" style="overflow: hidden">
+            <vaadin-tab>Tab-00</vaadin-tab>
+            <vaadin-tab>Tab-01</vaadin-tab>
+            <vaadin-tab>Tab-02</vaadin-tab>
+            <vaadin-tab>Tab-03</vaadin-tab>
+            <vaadin-tab>Tab-04</vaadin-tab>
+            <vaadin-tab>Tab-05</vaadin-tab>
+            <vaadin-tab>Tab-06</vaadin-tab>
+            <vaadin-tab>Tab-07</vaadin-tab>
+            <vaadin-tab>Tab-08</vaadin-tab>
+            <vaadin-tab>Tab-09</vaadin-tab>
+            <vaadin-tab>Tab-10</vaadin-tab>
+            <vaadin-tab>Tab-11</vaadin-tab>
+            <vaadin-tab>Tab-12</vaadin-tab>
+            <vaadin-tab>Tab-13</vaadin-tab>
+            <vaadin-tab>Tab-14</vaadin-tab>
+            <vaadin-tab>Tab-15</vaadin-tab>
+          </vaadin-tabs>
+        `);
+        div.style.height = '150px';
+        div.style.display = 'inline-flex';
+        document.body.appendChild(div);
+      });
+
+      after(() => {
+        document.body.removeChild(div);
+      });
+
+      it('vertical scroll', async () => {
+        await screenshot(div, 'vertical-scroll');
+      });
+
+      it('RTL vertical scroll', async () => {
+        await screenshot(div, 'rtl-vertical-scroll');
+      });
+    });
+  });
+});
---
+++ test/visual/material/item.test.js
@@ -0,0 +1,62 @@
+import '@vaadin/vaadin-item/theme/material/vaadin-item.js';
+import { createTest, fixture } from '../utils.js';
+
+const screenshot = createTest('item');
+
+describe('item', () => {
+  let div, element;
+
+  before(() => {
+    div = fixture();
+    document.body.appendChild(div);
+    div.style.setProperty('--_material-item-selected-icon-display', 'block');
+    div.style.padding = '0px';
+  });
+
+  beforeEach(() => {
+    element = document.createElement('vaadin-item');
+    element.textContent = 'Vaadin Item';
+    div.appendChild(element);
+  });
+
+  afterEach(() => {
+    div.removeChild(element);
+  });
+
+  it('basic', async () => {
+    await screenshot(div, 'basic');
+  });
+
+  it('focusable', async () => {
+    element.setAttribute('tabindex', '0');
+    await screenshot(div, 'focusable');
+  });
+
+  it('selected', async () => {
+    element.setAttribute('tabindex', '0');
+    element.setAttribute('selected', '');
+    await screenshot(div, 'selected');
+  });
+
+  it('RTL', async () => {
+    await screenshot(div, 'rtl');
+  });
+
+  it('RTL focusable', async () => {
+    element.setAttribute('tabindex', '0');
+    await screenshot(div, 'rtl-focusable');
+  });
+
+  it('RTL selected', async () => {
+    element.setAttribute('tabindex', '0');
+    element.setAttribute('selected', '');
+    await screenshot(div, 'rtl-selected');
+  });
+
+  it('multi line', async () => {
+    const second = document.createElement('div');
+    second.textContent = 'Second line';
+    element.appendChild(second);
+    await screenshot(div, 'multi-line');
+  });
+});
